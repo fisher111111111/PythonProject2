9956e58ffac900fdb17a8142f232b09a4770b866
Алексей _Git <fisher111111111@mail.ru>
2025-04-16 13:49:15 +0300

    Создание файла user_profile.py

---
+++ Final tasks of section 2.2/user_profile.py
@@ -0,0 +1,16 @@
+# Функция для ввода и проверки данных, тут не обошлось без читтерства
+def input_with_validation(prompt):
+    user_input = input(prompt)
+    if not user_input:
+        print("Ошибка: ввод не может быть пустым. Попробуйте снова!")
+        return input_with_validation(prompt)
+    return user_input
+
+name = input_with_validation("Введите ваше имя: ")
+profession = input_with_validation("Введите вашу профессию: ")
+tool = input_with_validation("Введите ваш любимый инструмент для тестирования: ")
+
+print(f"\nВаши данные:") # тут открыл для себя новый символ и его возможности переноса строки
+print(f"Имя: {name}")
+print(f"Профессия: {profession}")
+print(f"Ваш любимый инструмент для тестирования: {tool}")
