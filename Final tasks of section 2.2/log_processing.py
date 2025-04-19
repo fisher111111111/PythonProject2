f63b0edea87cf22803169fa50d0a81cdb1d95ac1
Алексей _Git <fisher111111111@mail.ru>
2025-04-19 23:47:48 +0300

    commmit log_processing.py

---
+++ Final tasks of section 2.2/log_processing.py
@@ -0,0 +1,15 @@
+# log_processing.py
+
+def main():
+
+    log_line = input("Введите строку лога: ")
+
+    processed_line = log_line.strip().replace("Ошибка", "Ошибка критическая")
+
+    words = processed_line.split()
+
+    print("Обработанная строка:", processed_line)
+    print("Разбитый текст:", words)
+
+if __name__ == "__main__":
+    main()
