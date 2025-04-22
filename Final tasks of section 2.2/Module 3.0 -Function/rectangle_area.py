35b0fe48fa261084e1ae802950c8c1d55848ac8c
Алексей _Git <fisher111111111@mail.ru>
2025-04-22 16:40:28 +0300

    commmit разных папок

---
+++ Final tasks of section 2.2/Module 3.0 -Function/rectangle_area.py
@@ -0,0 +1,9 @@
+def rectangle_area(length, width):
+    # Вычисление площади
+    area = length * width
+    # Форматированный вывод
+    print(f"Площадь прямоугольника с длиной {length} и шириной {width} равна {area}.")
+    return area
+
+# Пример вызова функции
+result = rectangle_area(5, 3)
