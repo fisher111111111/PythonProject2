cb71ef4f5b84935e75d04961395846e268008883
Алексей _Git <fisher111111111@mail.ru>
2025-04-22 16:47:54 +0300

    commmit convert_seconds.py

---
+++ Final tasks of section 2.2/Module 3.0 -Function/convert_seconds.py
@@ -0,0 +1,15 @@
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    return (hours, minutes)
+
+def main():
+    try:
+        seconds_input = int(input("Введите количество секунд: "))
+        hours, minutes = convert_seconds(seconds_input)
+        print(f"В {seconds_input} секундах содержится {hours} час(ов) и {minutes} минут(ы).")
+    except ValueError:
+        print("Пожалуйста, введите целое число.")
+
+if __name__ == "__main__":
+    main()
