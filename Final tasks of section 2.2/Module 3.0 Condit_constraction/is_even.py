96af357ccefd92a65b0181201a8e2365d04eafe3
Алексей _Git <fisher111111111@mail.ru>
2025-04-22 20:06:44 +0300

    commmit update check_grade

--- Final tasks of section 2.2/Module 3.0 Condit_constraction/is_even.py
+++ Final tasks of section 2.2/Module 3.0 Condit_constraction/is_even.py
@@ -1,7 +1,6 @@
 def is_even(number):
     return "чётным" if number % 2 == 0 else "нечётным"
 
-# Примеры использования функции
 number1 = -2
 result1 = is_even(number1)
 print(f"Число {number1} является {result1}.")
