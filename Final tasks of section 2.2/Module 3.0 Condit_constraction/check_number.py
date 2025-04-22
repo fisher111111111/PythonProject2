e2a1d45cf0dc2e1187df942275cbd68f73e189cb
Алексей _Git <fisher111111111@mail.ru>
2025-04-22 20:16:11 +0300

    commmit create check_number.py

---
+++ Final tasks of section 2.2/Module 3.0 Condit_constraction/check_number.py
@@ -0,0 +1,18 @@
+def check_number(number):
+    if number > 0:
+        if number % 2 == 0:
+            return f"Число {number} положительное и чётное."
+        else:
+            return f"Число {number} положительное и нечётное."
+    else:
+        return f"Число {number} отрицательное."
+
+# Примеры вызова функции
+result1 = check_number(8)
+print(result1)
+
+result2 = check_number(5)
+print(result2)
+
+result3 = check_number(-3)
+print(result3)
