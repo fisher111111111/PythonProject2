bcd9123dca2837935a1d358467e60323afa7c94f
Алексей _Git <fisher111111111@mail.ru>
2025-04-22 17:12:27 +0300

    commmit count_items.py

---
+++ Final tasks of section 2.2/Module 3.0 -Function/count_items.py
@@ -0,0 +1,15 @@
+def count_items(*args):
+    return len(args)  # Возвращаем количество переданных аргументов
+
+
+def main():
+
+    count1 = count_items(1, 2, 3, 4, 5)
+    print(f"Количество переданных элементов: {count1}.")
+
+    count2 = count_items("apple", "banana", "cherry")
+    print(f"Количество переданных элементов: {count2}.")
+
+
+if __name__ == "__main__":
+    main()
