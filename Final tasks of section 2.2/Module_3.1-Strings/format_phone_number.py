0f2a2f3d64a07fe59ecff60d3ad58deba9506e0f
Алексей _Git <fisher111111111@mail.ru>
2025-04-26 21:06:30 +0300

    rename fail

--- Final tasks of section 2.2/Module_3.1-Strings/format_phone_number.py
+++ Final tasks of section 2.2/Module_3.1-Strings/format_phone_number.py
@@ -4,4 +4,4 @@ def format_phone_number(digits):
     return f"({digits[:3]}) {digits[3:6]}-{digits[6:]}"
 
 
-print(format_phone_number("1234567890"))  # (123) 456-7890
+print(format_phone_number("1234567890"))
