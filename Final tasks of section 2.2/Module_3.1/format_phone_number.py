1b8d4cc760997770ff534517fb97f27d0386c003
Алексей _Git <fisher111111111@mail.ru>
2025-04-26 21:03:28 +0300

    create format_phone_number.py

---
+++ Final tasks of section 2.2/Module_3.1/format_phone_number.py
@@ -0,0 +1,7 @@
+def format_phone_number(digits):
+    if len(digits) != 10 or not digits.isdigit():
+        return "Неверный формат ввода"
+    return f"({digits[:3]}) {digits[3:6]}-{digits[6:]}"
+
+
+print(format_phone_number("1234567890"))  # (123) 456-7890
