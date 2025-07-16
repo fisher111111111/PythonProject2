bbced87f41995fcf219a9c53a804095f7ec7ef91
Алексей _Git <fisher111111111@mail.ru>
2025-07-16 10:26:32 +0300

    update 2 files

--- Module_4_2/src/constants/constant_of_URL.py
+++ Module_4_2/src/constants/constant_of_URL.py
@@ -2,3 +2,5 @@ from enum import Enum
 
 class ConstURL(Enum):
     BASE_URL = "https://restful-booker.herokuapp.com"
+    AUTH_ENDPOINT = "/auth"
+    BOOKING_ENDPOINT = "/booking"
--- Module_4_2/src/constants/constant_of_headers.py
+++ Module_4_2/src/constants/constant_of_headers.py
@@ -4,5 +4,6 @@ class ConstHeaders(Enum):
     HEADERS = (("Content-Type","application/json"), ("Accept","application/json"))
     CONTENT_TYPE = 'application/json'
     ACCEPT = 'application/json'
+    COOKIE = "token=abc123"
 
 headers_dict = dict(ConstHeaders.HEADERS.value)
