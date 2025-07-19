55adbeaac1ba7ebb55b7f4b44b6671fa1d2bf6be
Алексей _Git <fisher111111111@mail.ru>
2025-07-19 21:41:29 +0300

    добавил фикстуру auth_session  в conftest.py

--- Module_4_2/conftest.py
+++ Module_4_2/conftest.py
@@ -2,17 +2,20 @@ import pytest
 import requests
 import json
 from faker import Faker
-from constant import BASE_URL
-from constant import HEADERS
-from constant import VALID_CREDENTIALS
-from constant import INVALID_CREDENTIALS
+
+from PythonProject.constant import HEADERS, BASE_URL
+
+# from constant import BASE_URL
+# from constant import HEADERS
+# from constant import VALID_CREDENTIALS
+# from constant import INVALID_CREDENTIALS
 
 fake = Faker()
 
 @pytest.fixture(scope="session")
 def auth_token():
     response = requests.post(f"{BASE_URL}/auth", json=VALID_CREDENTIALS)
-    assert response.status_code == 200
+    assert response.status_code == 200, "Ошибочный статус-код"
     return response.json()["token"]
 
 @pytest.fixture(scope="session")
@@ -35,3 +38,16 @@ def booking_id(booking_data):
     response = requests.post(f"{BASE_URL}/booking", json=booking_data)
     assert response.status_code == 200
     return response.json()["bookingid"]
+
+@pytest.fixture(scope="session")
+def auth_session():
+    """Create session with authorization and return session`s object """
+    session = requests.Session()
+    session.headers.update(HEADERS)
+
+    auth_response = session.post(f'{BASE_URL}/auth', json={"username": "admin", "password": "password123"})
+    token = auth_response.json().get("token")
+    assert token is not None, "Токен в ответе отсутствует"
+
+    session.headers.update({"Cookie": f"token={token}"})
+    return session
