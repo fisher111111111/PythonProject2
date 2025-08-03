55ecd44ea8d8631c649f951dd21b87121143b617
Алексей _Git <fisher111111111@mail.ru>
2025-08-03 23:03:26 +0300

    обновление GraphQL приложения с добавлением БД и описанием к ее подключению

--- GraphQL_API_DB/schema.py
+++ GraphQL_API_DB/schema.py
@@ -1,6 +1,18 @@
+from __future__ import annotations
+
 import strawberry
 from typing import List, Optional
 from models import User as UserModel, Post as PostModel
+
+
+def to_dict(model_instance):
+    return {c.name: getattr(model_instance, c.name) for c in model_instance.__table__.columns}
+
+@strawberry.type
+class UserSummary:
+    id: strawberry.ID
+    name: str
+    email: Optional[str] = None
 
 @strawberry.type
 class Post:
@@ -10,10 +22,10 @@ class Post:
     link: Optional[str] = None
     author_id: strawberry.Private[int]
 
-    @strawberry.field()  # Вызов без параметров, добавлены скобки
-    def author(self, info) -> "User":
+    @strawberry.field()
+    def author(self, info) -> UserSummary:
         user = info.context['db'].query(UserModel).filter(UserModel.id == self.author_id).first()
-        return User(**user.dict()) if user else None
+        return UserSummary(**to_dict(user)) if user else None
 
 @strawberry.type
 class User:
@@ -24,7 +36,7 @@ class User:
     @strawberry.field()  # Добавлены скобки
     def posts(self, info) -> List[Post]:
         posts = info.context['db'].query(PostModel).filter(PostModel.author_id == int(self.id)).all()
-        return [Post(**post.dict()) for post in posts]
+        return [Post(**to_dict(post)) for post in posts]
 
 @strawberry.type
 class Query:
@@ -32,13 +44,13 @@ class Query:
     def users(self, info) -> List[User]:
         db = info.context['db']
         users = db.query(UserModel).all()
-        return [User(**user.dict()) for user in users]
+        return [User(**to_dict(user)) for user in users]
 
     @strawberry.field()  # Добавлены скобки
     def user(self, info, id: strawberry.ID) -> Optional[User]:
         db = info.context['db']
         user = db.query(UserModel).filter(UserModel.id == int(id)).first()
-        return User(**user.dict()) if user else None
+        return User(**to_dict(user)) if user else None
 
 @strawberry.type
 class CreateUserResult:
@@ -54,14 +66,16 @@ class DeleteUserResult:
 
 @strawberry.type
 class Mutation:
-    @strawberry.mutation()  # Добавлены скобки
+    @strawberry.mutation() # Добавлены скобки
     def create_user(self, info, name: str, email: Optional[str] = None) -> CreateUserResult:
         db = info.context['db']
         new_user_data = UserModel(name=name, email=email)
         db.add(new_user_data)
         db.commit()
         db.refresh(new_user_data)
-        return CreateUserResult(user=User(**new_user_data.dict()))
+
+        user_data = to_dict(new_user_data)  # конвертация в словарь
+        return CreateUserResult(user=User(**user_data))
 
     @strawberry.mutation()  # Добавлены скобки
     def update_user(self, info, id: strawberry.ID, name: Optional[str] = None, email: Optional[str] = None) -> UpdateUserResult:
@@ -75,7 +89,8 @@ class Mutation:
             user.email = email
         db.commit()
         db.refresh(user)
-        return UpdateUserResult(user=User(**user.dict()))
+        return UpdateUserResult(user=User(**to_dict(user)))
+
 
     @strawberry.mutation()  # Добавлены скобки
     def delete_user(self, info, id: strawberry.ID) -> DeleteUserResult:
