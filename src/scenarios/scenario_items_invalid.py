ffc83c97caf964b1c8c58d0d51c0277c6a04faa3
Алексей _Git <fisher111111111@mail.ru>
2025-11-09 12:45:19 +0300

    правка модуля scenario_items_invalid.py

--- src/scenarios/scenario_items_invalid.py
+++ src/scenarios/scenario_items_invalid.py
@@ -28,7 +28,7 @@ class BadScenarioCreate:
 #     item_session = requests.Session()
 #     api = BadScenarioCreate(item_session)
 #     result = api.create_too_long_title()
-#     print(f'wrong_title result = {result}')
+#     print(f' wrong_title result = {result}')
 
     def create_non_title(self):
         """Запрос на создание item"""
@@ -44,9 +44,9 @@ class BadScenarioCreate:
 #     item_session = requests.Session()
 #     api = BadScenarioCreate(item_session)
 #     result = api.create_non_title()
-#     print(f'non_title result = {result}')
+#     print(f' non_title result = {result}')
 
-    def create_bad_token(self):
+    def create_empty_token(self):
         """Запрос на создание item"""
         token_item = self.token
         token_item.headers['Authorization'] = None
@@ -62,19 +62,19 @@ class BadScenarioCreate:
 # if __name__ == "__main__":
 #     item_session = requests.Session()
 #     api = BadScenarioCreate(item_session)
-#     result = api.create_bad_token()
-#     print(f'wrong_title result = {result}')
+#     result = api.create_empty_token()
+#     print(f' wrong_title result = {result}')
 
 class BadScenariosItem:
     def __init__(self, item_session, api_client: ItemsApi):
-        self.item_session = item_session
+        self.session = item_session
         self.api_client = api_client
         self.generate_dates = RequestItem
-        self.session = item_session
         self.base_url = ConstURL.ITEMS_URL.value
         self.token =  AuthData().auth_token()
+        self.empty_token = AuthData().auth_empty_token()
 
-    def create_and_get_non_token(self):
+    def create_and_get_empty_token(self):
         '''
         Сценарий: создание итем, проверка ID созданного итем,
         затем удаление этого итем '''
@@ -83,12 +83,10 @@ class BadScenariosItem:
         id_item = item_create.get("id")
         print("Создан item =", id_item)
 
-        #2 Check item creating
-        token_item = self.token
+        #2 Get item with empty token
+        empty_token = self.empty_token
         response = self.session.get(f'{self.base_url}{id_item}',
-                                    headers=token_item.headers)
-        if response.status_code != 200:
-            response.raise_for_status()
+                                    headers=empty_token.headers)
         item = response.json()
         return item
 
@@ -99,5 +97,59 @@ class BadScenariosItem:
 #     # Создаем объект класса
 #     scenarios = BadScenariosItem(item_session, api_client)
 #     # Вызываем метод
-#     item_id = scenarios.create_check_delete_item()
+#     item_id = scenarios.create_and_get_empty_token()
+#     print(f"Обработанный item ID: {item_id}")
+
+    def create_and_update_empty_token(self):
+        '''
+        Сценарий: создание итем, проверка ID созданного итем,
+        затем удаление этого итем '''
+        # 1. Creating item
+        item_create= self.api_client.create_item()
+        id_item = item_create.get("id")
+        print("Создан item =", id_item)
+
+        #2 Update item with empty token
+        empty_token = self.empty_token
+        item_update = self.generate_dates.update_item_data()
+        response = self.session.put(f'{self.base_url}{id_item}',
+                                     headers=empty_token.headers,
+                                     json=item_update)
+        item = response.json()
+        return item
+
+# if __name__ == "__main__":
+#     # Инициализируем необходимые объекты
+#     item_session = requests.Session()  # здесь создайте или получите сессию с токеном
+#     api_client = ItemsApi(item_session)  # инициализация API клиента с нужными параметрами
+#     # Создаем объект класса
+#     scenarios = BadScenariosItem(item_session, api_client)
+#     # Вызываем метод
+#     item_id = scenarios.create_and_update_empty_token()
+#     print(f"Обработанный item ID: {item_id}")
+
+    def create_and_delete_empty_token(self):
+        '''
+        Сценарий: создание итем, проверка ID созданного итем,
+        затем удаление этого итем '''
+        # 1. Creating item
+        item_create= self.api_client.create_item()
+        id_item = item_create.get("id")
+        print("Создан item =", id_item)
+
+        #2 Delete item with empty token
+        empty_token = self.empty_token
+        response = self.session.delete(f'{self.base_url}{id_item}',
+                                       headers=empty_token.headers)
+        item_response = response.text
+        return item_response
+
+# if __name__ == "__main__":
+#     # Инициализируем необходимые объекты
+#     item_session = requests.Session()  # здесь создайте или получите сессию с токеном
+#     api_client = ItemsApi(item_session)  # инициализация API клиента с нужными параметрами
+#     # Создаем объект класса
+#     scenarios = BadScenariosItem(item_session, api_client)
+#     # Вызываем метод
+#     item_id = scenarios.create_and_delete_empty_token()
 #     print(f"Обработанный item ID: {item_id}")
